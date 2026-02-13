ebeb9f972e9c135f72eb15f6cc9da5cea957e07e
John Sierra <talk4aishik@gmail.com>
2026-02-14 01:42:56 +0530

    fix: add Node.js polyfills for mupdf browser compatibility

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,20 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
+  resolve: {
+    alias: {
+      // Provide empty polyfills for Node.js modules that mupdf tries to import
+      'node:fs': 'data:text/javascript,export default {}',
+      'module': 'data:text/javascript,export default {}',
+      'fs': 'data:text/javascript,export default {}',
+    }
+  },
+  optimizeDeps: {
+    exclude: ['mupdf'],
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  }
 })
